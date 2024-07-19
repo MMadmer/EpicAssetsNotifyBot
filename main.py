996ee80816e3e07190957f29ca92662f27d9f41e
Madmer <51472243+MMadmer@users.noreply.github.com>
2024-07-20 02:29:59 +0700

    Memory and CPU optimization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import aiohttp
 import discord
 from discord.ext import commands
-import requests
 from bs4 import BeautifulSoup
 import asyncio
 from datetime import datetime, timedelta
@@ -10,58 +10,31 @@ import os
 
 
 def get_month_name():
-    # This function returns the modified month name
     month_names = {
-        1: "Январские",
-        2: "Февральские",
-        3: "Мартовские",
-        4: "Апрельские",
-        5: "Майские",
-        6: "Июньские",
-        7: "Июльские",
-        8: "Августовские",
-        9: "Сентябрьские",
-        10: "Октябрьские",
-        11: "Ноябрьские",
-        12: "Декабрьские"
+        1: "Январские", 2: "Февральские", 3: "Мартовские", 4: "Апрельские",
+        5: "Майские", 6: "Июньские", 7: "Июльские", 8: "Августовские",
+        9: "Сентябрьские", 10: "Октябрьские", 11: "Ноябрьские", 12: "Декабрьские"
     }
     current_month = datetime.now().month
     return month_names[current_month]
 
 
-def get_free_assets():
-    # This function retrieves the list of free assets from the Epic Games Store
+async def get_free_assets():
     url = "https://www.unrealengine.com/marketplace/en-US/store"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/91.0.4472.124 Safari/537.36",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Referer": "https://www.unrealengine.com/",
-        "Origin": "https://www.unrealengine.com",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Pragma": "no-cache",
-        "Cache-Control": "no-cache"
+        "User-Agent": "Mozilla/5.0", "Accept-Language": "en-US,en;q=0.5",
+        "Referer": "https://www.unrealengine.com/", "Origin": "https://www.unrealengine.com",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
     }
 
-    session = requests.Session()
-    try:
-        response = session.get(url, headers=headers)  # Make a request to the store page
-        response.raise_for_status()  # Raise an exception for HTTP errors
-    except requests.RequestException as e:
-        print(f"Error fetching the page: {e}")
-        return None
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            if response.status != 200:
+                print(f"Error fetching the page: {response.status}")
+                return None
+            content = await response.text()
 
-    if response.status_code != 200:
-        print(response.status_code)
-        return None
-
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(content, 'html.parser')
     free_assets_section = soup.find('section', class_='assets-block marketplace-home-free')
     if not free_assets_section:
         print("Could not find the 'Free For The Month' section.")
@@ -71,15 +44,14 @@ def get_free_assets():
 
     assets = []
     for element in asset_elements:
-        name_element = element.find('h3')  # Find the name of the asset
-        link_element = element.find('a', href=True)  # Find the link to the asset
-        image_element = element.find('img')  # Find the image of the asset
+        name_element = element.find('h3')
+        link_element = element.find('a', href=True)
+        image_element = element.find('img')
         if name_element and link_element and image_element:
-            asset_name = name_element.text.strip()  # Get the text of the name element
-            # Get the href attribute of the link element
+            asset_name = name_element.text.strip()
             asset_link = "https://www.unrealengine.com" + link_element['href']
-            asset_image = image_element['src']  # Get the src attribute of the image element
-            assets.append({'name': asset_name, 'link': asset_link, 'image': asset_image})  # Add the asset to the list
+            asset_image = image_element['src']
+            assets.append({'name': asset_name, 'link': asset_link, 'image': asset_image})
 
     if not assets:
         print("No assets found in the 'Free For The Month' section.")
@@ -100,43 +72,37 @@ def load_data(filename):
             data = json.load(f)
             print(f"Loaded {len(data)} objects from {filename}.")
             return data
-
     print(f"{filename} not found. Load failed.")
-
     return []
 
 
 class EpicAssetsNotifyBot(commands.Bot):
     def __init__(self, command_prefix: str, token: str):
         intents = discord.Intents.default()
-        intents.message_content = True  # Enable message content intents
+        intents.message_content = True
         super().__init__(command_prefix=command_prefix, intents=intents)
         self.token = token
-        self.add_commands()  # Register commands
-
-        self.data_folder = self.data_folder = "/data/" if os.name != 'nt' else "data/"  # Folder for storing backup data
+        self.add_commands()
+        self.data_folder = "/data/" if os.name != 'nt' else "data/"
         self.subscribed_channels = load_data(
-            os.path.join(self.data_folder, 'subscribers_channels_backup.json'))  # Load subscribed channels from backup
+            os.path.join(self.data_folder, 'subscribers_channels_backup.json'))
         self.subscribed_users = load_data(
-            os.path.join(self.data_folder, 'subscribers_users_backup.json'))  # Load subscribed users from backup
+            os.path.join(self.data_folder, 'subscribers_users_backup.json'))
         self.assets_list = load_data(
-            os.path.join(self.data_folder, 'assets_backup.json'))  # Load assets list from backup
-        self.next_check_time = None  # Store the next check time
-        self.delete_after = 10  # Time after which the message will be deleted
-        self.backup_delay = 900  # Backup delay in seconds
+            os.path.join(self.data_folder, 'assets_backup.json'))
+        self.next_check_time = None
+        self.delete_after = 10
+        self.backup_delay = 900
 
     async def on_ready(self):
-        # This function is called when the bot is ready
         print(f'Logged in as {self.user}')
-        self.loop.create_task(self.set_daily_check())  # Start the daily check task
-        self.loop.create_task(self.backup_data())  # Start the backup task
+        self.loop.create_task(self.set_daily_check())
+        self.loop.create_task(self.backup_data())
 
     def run_bot(self):
-        # This function runs the bot with the provided token
         self.run(self.token)
 
     def add_commands(self):
-        # This function registers commands
         @self.command(name='sub')
         async def subscribe(ctx: commands.Context):
             if not is_admin(ctx) and not is_dm(ctx):
@@ -151,14 +117,15 @@ class EpicAssetsNotifyBot(commands.Bot):
                 self.subscribed_users.append({'id': user_id, 'shown_assets': False})
                 await ctx.send("Subscribed to asset updates")
 
-                # Check and send current assets if not shown
                 if self.assets_list and not self.subscribed_users[-1]['shown_assets']:
                     month_name = get_month_name()
                     message = f"## {month_name} ассеты от эпиков\n"
                     files = []
                     for asset in self.assets_list:
                         message += f"- [{asset['name']}](<{asset['link']}>)\n"
-                        image_data = requests.get(asset['image']).content
+                        async with aiohttp.ClientSession() as session:
+                            async with session.get(asset['image']) as resp:
+                                image_data = await resp.read()
                         files.append(discord.File(BytesIO(image_data), filename=f"{asset['name']}.png"))
                     user = await self.fetch_user(user_id)
                     await user.send(message, files=files)
@@ -172,14 +139,15 @@ class EpicAssetsNotifyBot(commands.Bot):
                 self.subscribed_channels.append({'id': channel_id, 'shown_assets': False})
                 await ctx.send(f"Subscribed to asset updates in: {ctx.channel.name}")
 
-                # Check and send current assets if not shown
                 if self.assets_list and not self.subscribed_channels[-1]['shown_assets']:
                     month_name = get_month_name()
                     message = f"## {month_name} ассеты от эпиков\n"
                     files = []
                     for asset in self.assets_list:
                         message += f"- [{asset['name']}](<{asset['link']}>)\n"
-                        image_data = requests.get(asset['image']).content
+                        async with aiohttp.ClientSession() as session:
+                            async with session.get(asset['image']) as resp:
+                                image_data = await resp.read()
                         files.append(discord.File(BytesIO(image_data), filename=f"{asset['name']}.png"))
                     channel = self.get_channel(channel_id)
                     await channel.send(message, files=files)
@@ -240,30 +208,31 @@ class EpicAssetsNotifyBot(commands.Bot):
             await asyncio.sleep(24 * 60 * 60)
 
     async def check_and_notify_assets(self):
-        new_assets = get_free_assets()  # Get the current list of free assets
-        if new_assets and new_assets != self.assets_list:  # Check if the list of assets has changed
-            self.assets_list = new_assets  # Update the stored list of assets
+        new_assets = await get_free_assets()
+        if new_assets and new_assets != self.assets_list:
+            self.assets_list = new_assets
             month_name = get_month_name()
             message = f"## {month_name} ассеты от эпиков\n"
             files = []
             for asset in new_assets:
-                message += f"- [{asset['name']}](<{asset['link']}>)\n"  # Format the message
-                image_data = requests.get(asset['image']).content
+                message += f"- [{asset['name']}](<{asset['link']}>)\n"
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(asset['image']) as resp:
+                        image_data = await resp.read()
                 files.append(discord.File(BytesIO(image_data), filename=f"{asset['name']}.png"))
 
             for channel in self.subscribed_channels:
                 channel_id = channel['id']
                 channel_obj = self.get_channel(channel_id)
                 if channel_obj:
-                    await channel_obj.send(message,
-                                           files=files)  # Send the message and files to the subscribed channels
+                    await channel_obj.send(message, files=files)
                     channel['shown_assets'] = True
 
             for user in self.subscribed_users:
                 user_id = user['id']
                 user_obj = await self.fetch_user(user_id)
                 if user_obj:
-                    await user_obj.send(message, files=files)  # Send the message and files to subscribed users
+                    await user_obj.send(message, files=files)
                     user['shown_assets'] = True
 
     async def backup_data(self):
@@ -281,7 +250,7 @@ class EpicAssetsNotifyBot(commands.Bot):
 
 
 if __name__ == '__main__':
-    TOKEN = os.environ["ASSETS_BOT_TOKEN"]  # Replace with your bot token
+    TOKEN = os.environ["ASSETS_BOT_TOKEN"]
     COMMAND_PREFIX = '/assets '
 
     bot = EpicAssetsNotifyBot(command_prefix=COMMAND_PREFIX, token=TOKEN)
